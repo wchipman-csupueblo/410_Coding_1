75facac75846fd705e1b9f99bd5b516cf93a0140
bill chipman <wrchipman@gmail.com>
2023-09-20 22:43:02 -0400

    Added the score logic

--- test_scores.py
+++ test_scores.py
@@ -14,6 +14,21 @@ while more_tests.lower() == "y":
     score_total = 0
     test_score = 0
 
+    while (test_score := input("Enter test score: ").lower()) != "end":
+        test_score = int(test_score)
+        if test_score >= 0 and test_score <= 100:
+            score_total += test_score
+            counter += 1
+        else:
+            print("Test score must be between 0 and 100. Try Again")
+
+    average_score = round(score_total / counter, 2)
+
+    print("====================")
+    print(f"Total Score: {score_total}"
+          f"\nAverage Score: {average_score}")
+    print()
+
     more_tests = input("Enter another set of test scores (y/n)? ")
 
 print("bye")
