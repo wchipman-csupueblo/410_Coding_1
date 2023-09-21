1fe9d5565d683e4df0e41b9a39a5be7ae1912496
bill chipman <wrchipman@gmail.com>
2023-09-20 21:57:18 -0400

    Updated gitignore. Added basic loop

---
+++ test_scores.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+print("The Test Scores Application")
+print()
+
+more_tests = "y"
+
+while more_tests.lower() == "y":
+    print("Enter test scores")
+    print("Enter 'end' to end input")
+    print("========================")
+
+    counter = 0
+    score_total = 0
+    test_score = 0
+
+    more_tests = input("Enter another set of test scores (y/n)? ")
+
+print("bye")
